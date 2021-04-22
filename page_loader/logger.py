a6679750923145a351f02638c24b57b434b22138
gato <alex.n.parshikov@gmail.com>
2021-04-22 12:57:23 +0300

    fix long string

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -3,7 +3,7 @@ import logging
 
 def setup(log_level='INFO'):
     logging.basicConfig(
-        format='[%(asctime)s] [%(levelname)s] [%(name)s] %(filename)s => %(message)s',
+        format='[%(asctime)s] [%(levelname)s] [%(name)s] %(filename)s => %(message)s',  # noqa: E501
         datefmt='%Y-%m-%d %H:%M:%S',
         level=logging.getLevelName(log_level),
         filename='loader.log',
